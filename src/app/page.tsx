24e95f72a7e787c3ed388ec2ddb985a491fd8461
Amir Hossein <a.qorbani.eng@gmail.com>
2025-01-25 16:38:39 +0330

    dasable strict mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,12 @@ import { useUser } from "@/components/sampleHooks/UseContext/UserContext";
 import { useEffect } from "react";
 
 export default function Home() {
-  // const { user, login, logout } = useUser();
-  // console.log("user : " + user);
-  // useEffect(() => {
-  //   login();
-  // }, [user]);
-  // console.log("user : " + user);
+  const { user, login, logout } = useUser();
+  console.log("user : " + user);
+  useEffect(() => {
+    login();
+  }, [user]);
+  console.log("user : " + user);
   return (
     <div className="flex flex-row justify-center items-center min-h-screen">
       {/* <UseState /> */}
