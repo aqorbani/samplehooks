b74057fa263a9a06b0442521d48713b03585af1d
Amir Hossein <a.qorbani.eng@gmail.com>
2025-02-20 22:28:32 +0330

    work with localstorage in next js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ import UseFormStatus from "@/components/sampleHooks/UseFormStatus/UseFormStatus"
 import UseAPI from "@/components/react19/use/UseAPI";
 import UseStreamingData from "@/components/react19/use2/UseStreamingData";
 import Server from "@/components/react19/server/Server";
-import CookieComponent from "@/components/test/CookieComponent";
+import LocalStorage from "@/components/Storage/localstorage/LocalStorage";
 
 export default function Home() {
   // const { user, login, logout } = useUser();
@@ -55,7 +55,8 @@ export default function Home() {
       {/* <UseAPI /> */}
       {/* <UseStreamingData /> */}
       {/* <Server /> */}
-      <CookieComponent />
+      {/* <CookieComponent /> */}
+      <LocalStorage />
     </div>
   );
 }
---
+++ src/components/Storage/localstorage/LocalStorage.tsx
@@ -0,0 +1,69 @@
+"use client";
+import React, { useState } from "react";
+
+const LocalStorage = () => {
+  const [data, setData] = useState<string>("");
+  const [key, setKey] = useState<string>("");
+
+  async function addLocalStorageHandler() {
+    localStorage.setItem("test", "ok");
+  }
+
+  async function getLocalStorageHandler() {
+    setData(localStorage.getItem("test") as string | "");
+  }
+
+  async function deleteLocalStorageHandler() {
+    localStorage.removeItem("test");
+    setData("");
+  }
+
+  async function clearLocalStorageHandler() {
+    localStorage.clear();
+    setData("");
+    setKey("");
+  }
+
+  async function getKeyLocalStorageHandler() {
+    setKey(localStorage.key(0) as string | "");
+  }
+
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <p className="m-9">Local Storage</p>
+      <button
+        onClick={addLocalStorageHandler}
+        className="m-2 p-3 bg-lime-300 rounded"
+      >
+        add
+      </button>
+      <button
+        onClick={getLocalStorageHandler}
+        className="m-2 p-3 bg-blue-300 rounded"
+      >
+        get
+        {data && <small>({data})</small>}
+      </button>
+      <button
+        onClick={deleteLocalStorageHandler}
+        className="m-2 p-3 bg-red-300 rounded"
+      >
+        delete
+      </button>
+      <button
+        onClick={clearLocalStorageHandler}
+        className="m-2 p-3 bg-purple-300 rounded"
+      >
+        clear local data
+      </button>
+      <button
+        onClick={getKeyLocalStorageHandler}
+        className="m-2 p-3 bg-emerald-300 rounded"
+      >
+        get key 0{key && <small>({key})</small>}
+      </button>
+    </div>
+  );
+};
+
+export default LocalStorage;
